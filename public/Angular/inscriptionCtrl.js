8a3f69ab82749680920d38c1306d1696f859e251
YanekC <yanekcolonge@hotmail.fr>
2018-05-24 23:09:05 +0200

    Ajout d'un peu de css plus debut gestion accompagnants

--- public/Angular/inscriptionCtrl.js
+++ public/Angular/inscriptionCtrl.js
@@ -1,6 +1,10 @@
 app.controller('inscriptionCtrl', function($scope, $http, $location, $cookies, $routeParams) {
 
 	var token = $cookies.get("token");
+	$scope.mail = [];
+	$scope.nom = [];
+	$scope.prenom = [];
+	$scope.tel = [];
     
     if(typeof token === 'undefined'){
     	alert("Veuillez vous connecter");
@@ -9,6 +13,11 @@ app.controller('inscriptionCtrl', function($scope, $http, $location, $cookies, $
     $http.get("http://localhost:3000/evenement/id="+$routeParams.id)
     .then(function (response) {
         $scope.infosEvent = response.data;
+
+        $http.get("http://localhost:3000/typePart/id="+response.data.idTypePart)
+    	.then(function (res) {
+    		$scope.nbAcc = new Array(res.data.maxAcc);
+    	});	
     });
     $http.get("http://localhost:3000/participe/idEvent="+$routeParams.id+"&idUser="+token)
     .then(function (response) {
@@ -35,42 +44,40 @@ app.controller('inscriptionCtrl', function($scope, $http, $location, $cookies, $
     }
 
     $scope.inscriptionAccomp = function(){
-    	var postInscription = {
-			"mail": $scope.mail,
-		    "nom": $scope.nom,
-		    "prenom": $scope.prenom,
-		    "tel": $scope.tel,
-		    "idTypePart" : $scope.infosEvent.idTypePart,
-		    "idAccompagnant" : token
-		};
 
-		$http.post("http://localhost:3000/addAccomp", postInscription)
-		.then(function(res){
-			
-			toPostEvent = {
-	    		"idUser" : $scope.mail,
-	    		"idEvent" : $routeParams.id
-	    	};
+    	$scope.mail.forEach(function(mail, index){
+    		var postInscription = {
+				"mail": mail,
+			    "nom": $scope.nom[index],
+			    "prenom": $scope.prenom[index],
+			    "tel": $scope.tel[index],
+			    "idTypePart" : $scope.infosEvent.idTypePart,
+			    "idAccompagnant" : token
+			};
 
-			$http.post("http://localhost:3000/inscriptionUser", toPostEvent)
+			$http.post("http://localhost:3000/addAccomp", postInscription)
 			.then(function(res){
-				alert("accompagnant Ajouté");
-			}, function(res){
-				$http.delete("http://localhost:3000/user/id="+$scope.mail)
+				
+				toPostEvent = {
+		    		"idUser" : mail,
+		    		"idEvent" : $routeParams.id
+		    	};
+
+				$http.post("http://localhost:3000/inscriptionUser", toPostEvent)
 				.then(function(res){
-					alert("accompagnant Supprimé");
+					alert("accompagnant Ajouté");
+				}, function(res){
+					$http.delete("http://localhost:3000/user/id="+mail)
+					.then(function(res){
+						alert("accompagnant Supprimé");
+					});
+					alert("Impossible d'ajouter l'accompagnant a l'event");
 				});
-				alert("Impossible d'ajouter l'accompagnant a l'event");
+
+
+			}, function(res){
+				alert("Le compte a cette adresse existe deja");
 			});
-
-
-		}, function(res){
-			alert("Le compte a cette adresse existe deja");
-		});
-
-		
-
-
-	    
+    	});
     }
 });
